7f36ade4612e8eefd66449da9eca6d22f286312f
mr.bashardragon@gmail.com <Student@SH-A148-04.utas.edu.om>
2024-12-04 15:24:58 +0400

    your message

--- client/src/Store/Store.js
+++ client/src/Store/Store.js
@@ -2,9 +2,35 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../Features/UserSlice";
 import postReducer from "../Features/PostSlice";
 
-export const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    posts: postReducer,
-  },
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "reduxstore", // The key to identify the persisted state in storage
+  storage, // The storage method (localStorage)
+  };
+
+
+const rootReducer = combineReducers({
+  users: usersReducer, // Manage users slice of the state
+  posts: postReducer, // Manage posts slice of the state
+  });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+reducer: persistedReducer, // Use the persisted reducer in the
+
 });
+
+const persistore = persistStore(store); // Create persistore forrehydration
+
+
+
+
+
+
+
+export { store, persistore };
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./Store/Store";
 import { Provider } from "react-redux";
+import { persistore } from "./Store/Store";
+import { PersistGate } from "redux-persist/integration/react";
 //import Register from "./Components/Register";
 //import Login from "./Components/Login";
 
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
+    <PersistGate loading={null} persistor={persistore}>
       <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>
 );
